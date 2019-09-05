f9ff140d86a494bbfa1241abae9d44acef9c9e45
Chris <listingslab@gmail.com>
2019-09-05 17:04:27 +1000

    feat(firebase)

--- src/App.js
+++ src/App.js
@@ -8,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        listingslab API Interface for hoomans
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
