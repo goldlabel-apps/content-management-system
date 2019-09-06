527b226bf4f4a3954867821bf18d121f79f3536c
Chris <listingslab@gmail.com>
2019-09-07 07:37:02 +1000

    feat(cra)

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-        listingslab API Interface for hoomans
+        listingslab API. Hooman Interface
         </p>
-        
       </header>
     </div>
   );
