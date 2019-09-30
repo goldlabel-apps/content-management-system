f549e78a4fcdcbd04491036175143f5cc6ca7bee
listingslab <listingslab@gmail.com>
2019-09-30 17:53:19 +1000

    Archiving this project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import packageJSON from '../package.json';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'typeface-muli';
+import muiTheme from './theme/mui';
+import { 
+  MuiThemeProvider, 
+  createMuiTheme 
+} from '@material-ui/core/styles';
 import { configureStore } from './store/configureStore';
 import { PersistGate } from 'redux-persist/es/integration/react';
 import { persistStore } from 'redux-persist';
@@ -13,48 +17,21 @@ import * as serviceWorker from './serviceWorker';
 
 console.log (`${packageJSON.name} ${packageJSON.version} (${process.env.REACT_APP_ENV})`);
 
-console.log (Date.now())
-
-
-/*
-  ## [TODO] Auto Update
-
-  - [STOP] mounting the app... till we're ready.
-    - put the ReactDom render into something connected to a saga
-    - make that saga do an async lookup of the version.json file
-    
-    ### Issue Description
-    
-    If the user has and old **redux shape** stored locally, 
-    it often breaks a new build which results in an unrecoverable 
-    error - the white screen of death.
-
-    WSOD means that react crashes, and when that user is using it as a PWA 
-    they can't interact with it in any way to force a refresh like they 
-    can traditionally on a browser.
-
-    To manage this, we create a version.json file in the public root. 
-    React reads the version, compares it to the version saved locally and if 
-    it isn't, it bloody should be.
-
-      - check if this is a new version of the app
-        - if so... localStorage.clear()
-        - send feedback to user `{appName} has been auto updated to version {appVersion}`
-        
-*/
-
-
 const store = configureStore();
 const getStore = () => { return store; };
 export { getStore };
 const persistor = persistStore(store);
 
+const theme = createMuiTheme( muiTheme );
+
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
-      <AppShell />
+      <MuiThemeProvider theme={theme}>
+        <AppShell />          
+      </MuiThemeProvider>
     </PersistGate>
   </Provider>
-, document.getElementById('agiliepwa'));
+, document.getElementById('react'));
 
 serviceWorker.register();
--- src/neo-containers/AppShell/AppShell.jsx
+++ src/neo-containers/AppShell/AppShell.jsx
@@ -1,63 +1,39 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import logo from '../../theme/svg/logo.svg';
-import muiTheme from '../../theme/mui';
-import { 
-    MuiThemeProvider, 
-    createMuiTheme 
-} from '@material-ui/core/styles';
 import { withStyles } from '@material-ui/core/styles';
-import cn from 'classnames';
+// import cn from 'classnames';
 import { styles } from './AppShell.Style';
-import {
-    LoginForm,
-    BottomAppBar,
-    Confirm,
-} from '../index';
-import {
-    Grid,
-    LinearProgress,
-} from '@material-ui/core/';
+// import {
+//     LoginForm,
+//     BottomAppBar,
+//     Confirm,
+// } from '../index';
+// import {
+//     Grid,
+//     LinearProgress,
+// } from '@material-ui/core/';
 
 class AppShell extends Component {  
     
     render (){
-        const { 
-            classes,
-            loading,
-        } = this.props;
-        const {
-            user,
-        } = this.props.store.auth;
+        // const { 
+        //     classes,
+        //     loading,
+        // } = this.props;
+        // const {
+        //     user,
+        // } = this.props.store.auth;
         
         // console.log ('loading', loading);
         // const loading = false;
 
-        const theme = createMuiTheme( muiTheme );
+        
         
         return (
-            <MuiThemeProvider theme={theme}>
-                <Confirm />
-                <React.Fragment>
-                    <div className={cn(classes.loading)}>
-                        { loading ? <LinearProgress /> : null}
-                    </div>
-                    <div className={cn(classes.app)}>
-                        <Grid container>
-                            <Grid item xs={12} className={cn(classes.iconGridItem)}>
-                                <img src={logo} alt={`logo`}/>
-                            </Grid>
-                        </Grid>
-                        { user === null ? 
-                            <LoginForm /> 
-                        : 
-                            <div>hello</div>
-                        }
-                    </div>
-                    <BottomAppBar />
-                </React.Fragment>
-            </MuiThemeProvider>
+            <React.Fragment>
+AppShell
+            </React.Fragment>
         );
     }
 }
